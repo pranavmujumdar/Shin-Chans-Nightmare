eb5001b98772471d3214f33505f98cf1184b4e06
Pranav Mujumdar <psmujumdar@gmail.com>
2020-01-30 22:20:46 -0500

    Main menu is hooked up with quit button and options button

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,4 +10,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void QuitGame()
+    {
+        Debug.Log("Quit Button Pressed!");
+        Application.Quit();
+    }
 }
